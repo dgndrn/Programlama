eaa0346faa36d849baca1ce3d3c4c65214e1edd7
dgndrn <celaldoganduran@gmail.com>
2023-11-10 13:44:54 +0300

    .

---
+++ TypeCasting/TypeCasting/TypeCasting.cpp
@@ -0,0 +1,31 @@
+﻿
+#include"lib.h"
+
+
+int main()
+{
+	int secim,veri;
+	cout << "1 Heksadesimal\n2 Oktal\n3 Binary\n";
+	cin >> secim;
+
+	cout << "Sayi giriniz:";
+	cin >> veri;
+	switch (secim) {
+
+	case 1:
+	cout <<	ToHex(veri);
+
+		break;
+	case 2:
+	cout <<	ToOctal(veri);
+		break;
+	case 3:
+	cout <<	ToBinary(veri);
+			break;
+
+	}
+
+
+
+}
+
