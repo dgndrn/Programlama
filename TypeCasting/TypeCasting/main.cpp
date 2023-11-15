98953e306a284d26d72daab5cf2476dda1e25d48
dgndrn <celaldoganduran@gmail.com>
2023-11-15 15:56:39 +0300

    .

--- TypeCasting/TypeCasting/main.cpp
+++ TypeCasting/TypeCasting/main.cpp
@@ -1,9 +1,18 @@
 
 #include"lib.h"
 
-
 int main()
-{
+{				// LPCSTR_to_LPCWSTR mainde tek harf bastýrýyor   strToLPCWSTR ikinci returnde bozuluyor  local degiskenden dolayi
+
+
+	LPCSTR nano1 = "zapzap";
+	wcout << LPCSTR_To_LPCWSTR(nano1);
+	
+	
+ 
+ return 0;
+
+
 	int secim, veri;
 	cout << "1 Heksadesimal\n2 Oktal\n3 Binary\n";
 	cin >> secim;
